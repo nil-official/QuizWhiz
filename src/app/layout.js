867831faf94f36ef895e1f868aaa1582c44e1b4f
nil-official <niladrichakraborty16@gmail.com>
2025-04-27 00:33:28 +0530

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ReduxProvider } from './providers';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <ReduxProvider>
           {children}
         </ReduxProvider>
